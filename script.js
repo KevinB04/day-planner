a8f30e5d7741dc0fecf47afcd4f428e6c838ecb3
Kevin Buttimer <kbuttimer4@gmail.com>
2020-12-16 00:19:42 -0500

    updating files

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const now = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log("Hello world");
 
+$(document).ready(function () {
 
+    var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM",];
 
+for (var i=0; i < times.length; i++) {
+    var 
+}
+
+$(".saveBtn").on("click", function() {
+    
+}
